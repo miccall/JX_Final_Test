6452e0576ef4be7d1c8117a97eeed102873dbe2b
mosoup <34511494+mosoup@users.noreply.github.com>
2017-12-15 00:59:47 +0800

    3

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -15,16 +15,18 @@ int main()
     int i, j;
     int nums[10][5];
     int a, b;
+    cout << "请在每行输入5个数字，数字之间用空格隔开，程序将为您生成10行5列的数组！" << endl;
+
     for(i = 0; i < 10; i++)
     {
-
+        cout << "第" << i + 1 << "行：" << endl;
         for(j = 0; j < 5; j++)
         {
             cin >> nums[i][j];
         }
         cout << endl;
     }
-    cout << "二维数组的输出为：" << endl;
+    cout << "你创建的二维数组为：" << endl;
     for(a = 0; a < 10; a++)
     {
 
